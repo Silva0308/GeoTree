2a626465cc1f9a1f51f253bdb2e9c06521b80e3a
Silva0308 <travinina_valery@mail.ru>
2022-12-22 14:14:00 +0300

    first commit

---
+++ program.java
@@ -0,0 +1,6 @@
+public class program {
+    public static void main(String[] args) {
+        Human pers1 = new Human("Иван", "Иванов", "Петрович", GenderType.MALE, 1977, 11, 3);
+        System.out.println(pers1);
+    }
+}
